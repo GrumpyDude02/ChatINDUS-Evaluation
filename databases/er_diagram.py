a05ef4a680b045c41c1e2fa54fc8b1e621f5aff3
unknown <youssefbakouch02@gmail.com>
2025-06-26 16:06:33 +0200

    added a script to get er diagrams

--- databases/er_diagram.py
+++ databases/er_diagram.py
@@ -7,8 +7,6 @@ parser.add_argument('output_file',type=str,help='Output file')
 
 args = parser.parse_args()
 
-os.environ["PATH"] += os.pathsep + r"C:\Program Files\Graphviz\bin"
-
 # Draw from database
 database_path = os.path.abspath(args.database_path)
 render_er(f"sqlite:///{database_path}", args.output_file)
